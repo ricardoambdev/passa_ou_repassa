8457ac8624c0576584ffb13cd048c04477f730d3
Ricardo Amb <ricardoamb.dev@hotmail.com>
2026-07-08 10:54:10 -0300

    permite qualquer IP no dev server

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["10.10.10.219"],
+  allowedDevOrigins: ["*.*.*.*"],
 };
 
 export default nextConfig;
